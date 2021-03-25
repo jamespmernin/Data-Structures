ecc7d24c5e49f559577cd0cfac8989f9c88301a2
James Mernin <jamespmernin@gmail.com>
2021-03-25 13:36:34 -0400

    added set function to singly linkedl ist

--- DataStructures.js
+++ DataStructures.js
@@ -74,4 +74,12 @@ class SinglyLinkedList {
         }
         return current;
     }
+    set(index, val) { // change the value of the node at the given index, return whether it succeeded
+        let foundNode = this.get(index);
+        if (foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
 }
