f999e61494457b5c869578e96f5f0c5aa4923ea7
James Mernin <jamespmernin@gmail.com>
2021-03-25 14:54:48 -0400

    implemented stack

--- DataStructures.js
+++ DataStructures.js
@@ -272,4 +272,26 @@ class Stack {
         this.last = null;
         this.size = 0;
     }
+    push(value) { // add new node to top of stack and return the new length
+        let newNode = new StackNode(value);
+        if (!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+    pop() { // remove last item added to stack, return that item
+        if (!this.first) return null;
+        let temp = this.first;
+        if (this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.value;
+    }
 }
