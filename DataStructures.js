e9abcb4b94d531387e2aa2e6bb38463299a6fb56
James Mernin <jamespmernin@gmail.com>
2021-03-25 13:46:54 -0400

    Add reverse to singly linked list

--- DataStructures.js
+++ DataStructures.js
@@ -104,4 +104,18 @@ class SinglyLinkedList {
         this.length--;
         return removed;
     }
+    reverse() { // reverse the direction of the list and return that list
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        let next;
+        let prev = null;
+        for (let i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
+    }
 }
