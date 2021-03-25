c7a211d6227694e0ef138769637d08b47bc36e8d
James Mernin <jmernin1992@gmail.com>
2021-03-25 12:57:55 -0400

    Adding data structures file

---
+++ DataStructures.js
@@ -0,0 +1,17 @@
+class SingleNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val) {
+        
+    }
+}
