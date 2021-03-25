c57e1cddd0cab38ed4a12a37c8ce623d4ea79920
James Mernin <jamespmernin@gmail.com>
2021-03-25 14:06:29 -0400

    added get function to doubly linked list

--- DataStructures.js
+++ DataStructures.js
@@ -189,4 +189,24 @@ class DoublyLinkedList {
         this.length--;
         return oldHead;
     }
+    get(index) { // return the node at the index provided
+        if (index < 0 || index >= this.length) return null;
+        let count, current;
+        if (index <= this.length / 2) {
+            count = 0;
+            current = this.head;
+            while (count !== index) {
+                current = current.next;
+                count++;
+            }
+        } else {
+            count = this.length - 1;
+            current = this.tail;
+            while (count !== index) {
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+    }
 }
