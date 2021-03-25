ec0b5e3f7ff88105f17a6e39fc4d440e600eabd9
James Mernin <jamespmernin@gmail.com>
2021-03-25 13:21:17 -0400

    Added pop to singly linked list

--- DataStructures.js
+++ DataStructures.js
@@ -29,4 +29,17 @@ class SinglyLinkedList {
         this.length++;
         return this;
     }
+    pop() {
+        if (!this.head) return undefined;
+        let current = this.head;
+        let newTail = current;
+        while(current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        return current;
+    }
 }
