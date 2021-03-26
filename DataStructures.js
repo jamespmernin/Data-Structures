8ed142d1e85465abf90b7f939722d032a361ce72
James Mernin <jamespmernin@gmail.com>
2021-03-26 14:27:39 -0400

    implemented insert on BST

--- DataStructures.js
+++ DataStructures.js
@@ -346,4 +346,28 @@ class BST {
     constructor() {
         this.root = null;
     }
+    insert(value) {
+        let newNode = new BSTNode(value);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        }
+        let current = this.root;
+        while (true) {
+            if (value === current.value) return undefined;
+            if (value < current.value) {
+                if (!current.left) {
+                    current.left = newNode;
+                    return this;
+                }
+                current = current.left;
+            } else {
+                if (!current.right) {
+                    current.right = newNode;
+                    return this;
+                }
+                current = current.right;
+            }
+        }
+    }
 }
