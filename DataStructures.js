1ae94fe646bc2adcb83e0b22c761af6b9c7b0ad9
James Mernin <jamespmernin@gmail.com>
2021-03-29 15:27:06 -0400

    implemented priority queue

--- DataStructures.js
+++ DataStructures.js
@@ -550,3 +550,72 @@ class MinBinaryHeap {
         }
     }
 }
+
+class PriorityNode {
+    constructor(value, priority) {
+        this.value = value;
+        this.priority = priority;
+    }
+}
+
+class PriorityQueue {
+    constructor() {
+        this.values = [];
+    }
+    enqueue(value, priority) {
+        let newNode = new PriorityNode(value, priority);
+        this.values.push(newNode);
+        this.bubbleUp();
+    }
+    bubbleUp() {
+        let index = this.values.length - 1;
+        const element = this.values[index];
+        while (index > 0) {
+            let parentIndex = Math.floor((index - 1) / 2);
+            let parent = this.values[parentIndex];
+            if (element.priority >= parent.priority) break;
+            this.values[parentIndex] = element;
+            this.values[index] = parent;
+            index = parentIndex;
+        }
+    }
+    dequeue() {
+        const min = this.values[0];
+        const end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this.bubbleDown();
+        }
+        return min;
+    }
+    bubbleDown() {
+        let i = 0;
+        const element = this.values[0];
+        const length = this.values.length;
+        while(true) {
+            let l = 2 * i + 1;
+            let r = l + 1;
+            let left, right;
+            let swap = null;
+            if (l < length) {
+                left = this.values[l];
+                if (left.priority < element.priority) {
+                    swap = l;
+                }
+            }
+            if (r < length) {
+                right = this.values[r];
+                if (
+                    (swap === null && right.priority < element.priority) ||
+                    (swap !== null && right.priority < left.priority)
+                ) {
+                    swap = r;
+                }
+            }
+            if (swap === null) break;
+            this.values[i] = this.values[swap];
+            this.values[swap] = element;
+            i = swap;
+        }
+    }
+}
