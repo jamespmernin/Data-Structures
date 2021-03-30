18970754e832c762716567d7694fe0b515450e4f
James Mernin <jamespmernin@gmail.com>
2021-03-30 15:37:21 -0400

    implement remove functions for graph

--- DataStructures.js
+++ DataStructures.js
@@ -693,8 +693,19 @@ class Graph {
     addVertex(vertex) {
         if(!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
     }
+    removeVertex(vertex) {
+        while(this.adjacencyList[vertex].length) {
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex];
+    }
     addEdge(v1, v2) {
         this.adjacencyList[v1].push(v2);
         this.adjacencyList[v2].push(v1);
+    }
+    removeEdge(v1, v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2);
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1);
     }
 }
