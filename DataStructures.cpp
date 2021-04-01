418da074b6bf3eb0456a102e134efd1dbacbeaa7
James Mernin <jamespmernin@gmail.com>
2021-04-01 10:47:28 -0400

    singly linked list in C++

--- DataStructures.cpp
+++ DataStructures.cpp
@@ -6,7 +6,24 @@
 #include <iostream>
 
 // Singly Linked Lists
-struct SingleNode {
-    int data;
-    struct Node *next;
+class SinglyLinkedList {
+    struct SingleNode {
+        int data;
+        struct SingleNode *next;
+    };
+    struct SingleNode* head = NULL;
+    void insert(int new_data) {
+        struct SingleNode* new_node = (struct SingleNode*) malloc(sizeof(struct SingleNode));
+        new_node->data = new_data;
+        new_node->next = head;
+        head = new_node;
+    }
+    void display() {
+        struct SingleNode* ptr;
+        ptr = head;
+        while (ptr != NULL) {
+            std::cout<< ptr->data <<" ";
+            ptr = ptr->next;
+        }
+    }
 };
